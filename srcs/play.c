e3868b88b451c4320750b55de393f859d55a57ae
Louis CALVIE <lcalvie@e2r12p12.clusters.42paris.fr>
2022-05-23 20:28:44 +0200

    ENFIN REUSSI A CORRIGER L EFFET DE DISORSION

--- srcs/play.c
+++ srcs/play.c
@@ -6,7 +6,7 @@
 /*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 14:22:59 by lcalvie           #+#    #+#             */
-/*   Updated: 2022/05/19 15:34:22 by lcalvie          ###   ########.fr       */
+/*   Updated: 2022/05/23 20:27:55 by lcalvie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,23 @@
 
 void	print_game(t_graph *graph)
 {
-	int i;
-	double angle;
-	double fish_eye_correction;
-	double d;
-	double x_wall;
+	int	i;
+	double	angle;
+	double	fish_eye_correction;
+	double	d;
+	double	x_wall;
 
 	i = -1;
 	//printf("angle de base = %f\n", graph->game.angle_vision);
 	while (++i < WIDTH)
 	{
-		angle = graph->game.angle_vision + ((double)i / (WIDTH - 1) - 0.5) * FOV;
+		angle = graph->game.angle_vision + atan(((double)i / (WIDTH - 1) - 0.5)) * FOV / (2 * atan(0.5));
 		if (angle < 0)
 			angle += 360;
 		else if (angle > 360)
 			angle -= 360;
 		//printf("----------------- check angle %f  -----------------------\n", angle);
-		fish_eye_correction = ((double)i / (WIDTH - 1) - 0.5) * FOV;
-		// A VERIFIER avec un FOV > 90 !!
-		while (fish_eye_correction >= 90)
-			fish_eye_correction -= 90;
-		while (fish_eye_correction <= -90)
-			fish_eye_correction += 90;
+		fish_eye_correction = atan(((double)i / (WIDTH - 1) - 0.5)) * FOV / (2 * atan(0.5));
 		//printf("correction : %f\n", fish_eye_correction);
 		d = wall_distance(graph, angle, &x_wall);
 		//printf("DISTANCE TO WALL = %f\n", d);
