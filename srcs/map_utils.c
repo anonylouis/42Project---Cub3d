5c61b44c29c3f846bdaa4b8db7c9cc0fcbd7aec3
mrahmani <mrahmani@student.42.fr>
2022-06-01 21:27:27 +0200

    parsing map with spaces

--- srcs/map_utils.c
+++ srcs/map_utils.c
@@ -6,34 +6,53 @@
 /*   By: mrahmani <mrahmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 15:42:20 by mrahmani          #+#    #+#             */
-/*   Updated: 2022/05/31 22:38:58 by mrahmani         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:07:36 by mrahmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+int check_spaces(char **s, int start, int *i)
+{
+    int j;
+
+    j = 0;
+    while (s[*i][j] != '\0')
+    {
+        if (*i == start || *i == count(s) - 1)
+        {
+            if (s[*i][j] != '1' && s[*i][j] != ' ')
+                return (0);
+        }
+        else
+        {
+            if (s[*i][j] == ' ')
+            {
+                if ((s[*i + 1][j] != ' ' && s[*i + 1][j] != '1') 
+                || (s[*i - 1][j] != '1' && s[*i - 1][j] != ' ') 
+                || (ft_strlen(s[*i]) - 1 > j && ((s[*i][j + 1] != '1' 
+                && s[*i][j + 1] != ' ') 
+                || (s[*i][j - 1] != '1' && s[*i][j - 1] != ' '))))
+                    return (0);
+            }
+        }
+        j++;
+    }
+    return (1);
+}
+
 int check_walls(char **s, int start)
 {
     int i;
-    int j;
     int len;
 
     i = start;
-    j = 0;
     while (s[i] != NULL)
     {
-        j = 0;
-        while (s[i][j] != '\0')
-        {
-            if (i == start || i == count(s) - 1)
-            {
-                if (s[i][j] != '1' && s[i][j] != ' ')
-                    return (0);
-            }
-            j++;
-        }
         len = ft_strlen(s[i]);
-        if (s[i][0] != '1' || s[i][len - 1] != '1')
+        if (!check_spaces(s, start, &i))
+            return (0);
+        if ((s[i][0] != '1' && s[i][0] != ' ') || (s[i][len - 1] != ' ' && s[i][len - 1] != '1'))
             return (0);
         i++;
     }
@@ -73,7 +92,6 @@ int check_empty_lines(char **s, int start)
     }
     return (1);
 }
-
 
 char **get_map(char *path)
 {
